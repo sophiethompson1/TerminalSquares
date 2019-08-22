7da31c2ad3b9da8708b8c181ac4fe9182c544e99
Sophie <sophie.thompson@imperial.ac.uk>
2019-08-23 00:37:23 +0100

    Got a working cycle

--- src/Paper.java
+++ src/Paper.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Paper {
   private final int width;
@@ -44,35 +45,65 @@ public class Paper {
   }
 
   public void makeMark(char c, int x, int y) {
-    plane[y*2 - 1][x*2 - 1] = c;
+    plane[y][x] = c;
   }
 
   public boolean isCorner(int x, int y) {
-    return plane[y*2 - 1][x*2 - 1] == 'o';
+    return plane[y][x] == 'o';
   }
 
-  public boolean isMark(char c, int x, int y) {
-    return plane[y][x] == c;
+  public boolean isBlank(int x, int y) {
+    return plane[y][x] == ' ';
   }
 
-  public boolean needsToBeFilled(char c, int x, int y) {
-    assert ()
+  public boolean needsToBeFilled(int x, int y) {
+    //need to write an assert comment here
+    return plane[y][x - 1] != ' ' && plane[y][x + 1] != ' ' && plane[y - 1][x] != ' ' && plane[y + 1][x] != ' ';
+
   }
 
-  public void fillIn() {
-    for (int y = 1; y < height*(2 - 1); y++) { //may as well start from 1
-      for (int x = 1; x < width*(2 - 1); x++) {
-        if ()
+  public void fillIn(char c) {
+    for (int y = 1; y < height*(2 - 1); y = y + 2) { //may as well start from 1
+      for (int x = 1; x < width*(2 - 1); x = x + 2) {
+        if (needsToBeFilled(x, y)) {
+          makeMark(c, x, y);
+        }
       }
     }
   }
 
   public boolean isFinished() {
-
+    for (int y = 1; y < height*(2 - 1); y = y + 2) { //may as well start from 1
+      for (int x = 1; x < width * (2 - 1); x = x + 2) {
+        if (plane[y][x] == ' '){
+          return false;
+        }
+      }
+    }
+    return true;
   }
+
+  public boolean isMiddle(int x, int y) {
+    return x % 2 == 1 && y % 2 == 1; //depends if you index beginning from 1 or 0
+  }
+
   public static void main(String[] args) {
     Paper paper = new Paper(3, 3);
+    Scanner input = new Scanner(System.in);
+    int x = 0;
+    int y = 0;
+    while(!paper.isFinished()) {
+      while (paper.isCorner(x, y) || !paper.isBlank(x, y) || paper.isMiddle(x, y)) { //if both even then it is a corner
+        System.out.println("X: ");
+        x = Integer.parseInt(input.nextLine());
+        System.out.println("Y: ");
+        y = Integer.parseInt(input.nextLine());
+      }
+      paper.makeMark('B', x, y);
+      paper.fillIn('B');
+      paper.printPaper();
+    }
 
-  }
+  } // this cycle works
 
 }
